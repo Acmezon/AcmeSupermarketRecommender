46c870d50b7fc2c52a3647e746039d51755eae3f
Ale Sánchez <alesanchezmedina@gmail.com>
2016-01-22 12:55:48 +0100

    Refactorizado el código para separar la configuración de la recomendación

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -7,6 +7,9 @@ import org.apache.spark.SparkContext
 import com.mongodb.casbah.MongoClient
 import com.mongodb.casbah.commons.MongoDBObject
 import scala.util.Random
+import breeze.macros.expand.args
+import org.apache.spark.mllib.recommendation.ALS
+import breeze.macros.expand.args
 
 object Functions {
   def loadUserRatings(userId: Int, sc: SparkContext): RDD[Rating] = {
@@ -14,7 +17,7 @@ object Functions {
     var mongoRatesColl = mongoRatesConnection("Acme-Supermarket")("rates");
     //Load my ratings
     val query = MongoDBObject("customer_id" -> userId);
-    val fields = MongoDBObject("rate" -> 1, "product_id" -> 1);
+    val fields = MongoDBObject("value" -> 1, "product_id" -> 1, "customer_id" -> 1);
     val rows = mongoRatesColl.find(query, fields).toSeq;
     
     val rows_rdd = sc.parallelize(rows);
@@ -22,7 +25,7 @@ object Functions {
     val ratings = rows_rdd.map { row =>
       Rating(Integer.parseInt(row.get("customer_id").toString()), 
               Integer.parseInt(row.get("product_id").toString()), 
-              Integer.parseInt(row.get("rate").toString()));
+              Integer.parseInt(row.get("value").toString()));
     }.filter(_.rating > 0)
     if (ratings.isEmpty) {
       mongoRatesConnection.underlying.close();
@@ -36,40 +39,40 @@ object Functions {
   }
   
   def loadRatings(sc: SparkContext): RDD[(Long, Rating)] = {
-    var r = Random;
-    
     val mongoRatesConnection = MongoClient();
     var mongoRatesColl = mongoRatesConnection("Acme-Supermarket")("rates");
     //Load every other ratings
     val query = MongoDBObject.empty;
-    val fields = MongoDBObject("rate" -> 1, "product_id" -> 1);
+    val fields = MongoDBObject("value" -> 1, "product_id" -> 1, "customer_id" -> 1);
     val rows = mongoRatesColl.find(query, fields).toSeq;
     
     val rows_rdd = sc.parallelize(rows);
     
     val ratings = rows_rdd.map { row =>
+      var r = Random;
       (r.nextLong() % 10, Rating(Integer.parseInt(row.get("customer_id").toString()), 
                                   Integer.parseInt(row.get("product_id").toString()), 
-                                  Integer.parseInt(row.get("rate").toString())));
+                                  Integer.parseInt(row.get("value").toString())));
     }
     
-    mongoRatesConnection.underlying.close();
-    mongoRatesColl = null;
-    ratings
+    //mongoRatesConnection.underlying.close();
+    //mongoRatesColl = null;
+    
+    return ratings;
   }
   
-  def loadProducts(sc: SparkContext): Map[Int, String] = {
+  def loadProducts(sc: SparkContext): Array[Int] = {
     val mongoProductConnection = MongoClient();
     var mongoProductColl = mongoProductConnection("Acme-Supermarket")("products");
     
     val query = MongoDBObject.empty;
-    val fields = MongoDBObject("name" -> 1);
+    val fields = MongoDBObject("_id" -> 1);
     
     val rows = mongoProductColl.find(query, fields).toSeq;
     
     val products = rows.map { row =>
-      (Integer.parseInt(row.get("_id").toString()), row.get("name").toString())
-    }.toMap
+      (Integer.parseInt(row.get("_id").toString()))
+    }.toArray;
     
     mongoProductConnection.underlying.close();
     mongoProductColl = null;
@@ -86,19 +89,142 @@ object Functions {
   }
   
   def predictForUser(model: MatrixFactorizationModel, userId: Int, sparkContext: SparkContext): RDD[Rating] = {
-    val notRatedMovies = getNotRatedMoviesByUser(userId, sparkContext);
-    val prediction = model.predict(notRatedMovies)
-    
-    prediction
+    val notRatedProducts = getNotRatedProductsByUser(userId, sparkContext);
+        
+    val prediction = model.predict(notRatedProducts)
+        
+    prediction;
   }
   
-  def getNotRatedMoviesByUser(userId: Int, sparkContext: SparkContext): RDD[(Int, Int)] = {
-    val allFilms = loadProducts(sparkContext)
-    val ratedFilms = loadUserRatings(userId, sparkContext).map(_.product).collect().toSet
+  def getNotRatedProductsByUser(userId: Int, sparkContext: SparkContext): RDD[(Int, Int)] = {
+    val allProducts = loadProducts(sparkContext)
+    val ratedProducts = loadUserRatings(userId, sparkContext).map(_.product).collect().toSet
     
-    val candidates = sparkContext.parallelize(allFilms.keys.filter(!ratedFilms.contains(_)).toSeq)
+    val candidates = sparkContext.parallelize(allProducts.filter(!ratedProducts.contains(_)).toSeq)
     
-    val notRatedMovies = candidates.map((userId, _))
-    notRatedMovies
+    val notRatedProducts = candidates.map((userId, _))
+    
+    notRatedProducts;
+  }
+  
+  def makeRecommendation(sc: SparkContext, user_id: Int) = {
+    val configConnection = MongoClient();
+    var configColl = configConnection("Acme-Supermarket-Recommendations")("config");
+   
+    val config = configColl.find().toSeq;
+    
+    if(config.length == 0) {
+      println("No hay configuraciones en la BD. Ejecuta el sistema con la id de usuario -1 para poner configucaciones.");
+      sys.exit(1);
+    }
+    
+    val numPartitions = 6;
+    val rank = config(0).get("rank").toString().toInt;
+    val numIter = config(0).get("numIter").toString().toInt;
+    
+    val lambda = config(0).get("numPartitions").toString().toDouble;
+    
+    // load personal ratings
+    val myRatingsRDD = Functions.loadUserRatings(user_id, sc)
+    
+    // load ratings and products
+    val ratings = Functions.loadRatings(sc)
+    
+    val training = ratings.filter(x => x._1 <= 3)
+      .values
+      .union(myRatingsRDD)
+      .repartition(numPartitions)
+      .cache()
+      
+    val model = ALS.train(training, rank, numIter, lambda);
+    
+    val prediction = Functions.predictForUser(model, user_id, sc).collect().sortBy(_.rating).take(10)
+    
+    val predictionsConnection = MongoClient();
+    var predictionsColl = predictionsConnection("Acme-Supermarket-Recommendations")("recommendations");
+    
+    prediction.foreach { rating =>
+      val recommendation = MongoDBObject(
+          "customer_id" -> rating.user,
+          "product_id" -> rating.product,
+          "rating" -> rating.rating
+      );
+      
+      predictionsColl += recommendation;
+    }
+    
+    predictionsConnection.underlying.close();
+    predictionsColl = null;
+    // clean up
+  }
+  
+  def configureParameters(sc: SparkContext, user_id : Int) = {
+    val configConnection = MongoClient();
+    var configColl = configConnection("Acme-Supermarket-Recommendations")("config");
+    
+    // load personal ratings
+    val myRatingsRDD = Functions.loadUserRatings(user_id, sc)
+    
+    // load ratings and products
+    val ratings = Functions.loadRatings(sc)
+
+    val numPartitions = 6;
+    val training = ratings.filter(x => x._1 <= 3)
+      .values
+      .union(myRatingsRDD)
+      .repartition(numPartitions)
+      .cache()
+    val validation = ratings.filter(x => x._1 > 3 && x._1 <= 4)
+      .values
+      .repartition(numPartitions)
+      .cache()
+    val test = ratings.filter(x => x._1 > 4).values.cache()
+    
+    val numValidation = validation.count()
+
+    val ranks = List(8, 12)
+    val lambdas = List(1.0, 10.0)
+    val numIters = List(10, 20)
+    
+    var bestValidationRmse = Double.MaxValue
+    var bestRank = 0
+    var bestLambda = -1.0
+    var bestNumIter = -1
+    for (rank <- ranks; lambda <- lambdas; numIter <- numIters) {
+      val model = ALS.train(training, rank, numIter, lambda);
+      
+      val validationRmse = Functions.computeRmse(model, validation, numValidation);
+      
+      if (validationRmse < bestValidationRmse) {
+        bestValidationRmse = validationRmse
+        bestRank = rank
+        bestLambda = lambda
+        bestNumIter = numIter
+      }
+    }
+        
+    val query = MongoDBObject.empty;
+    val fields = MongoDBObject("_id" -> 1);
+    val rows = configColl.find(query, fields).toSeq;
+    
+    val bestValues = MongoDBObject(
+      "rank" -> bestRank,
+      "numIter" -> bestNumIter,
+      "lambda" -> bestLambda
+    );
+    if ( rows.length == 0 ){
+      configColl += bestValues;
+    } else {
+      var conf_id = rows(0).get("_id").toString();
+    
+      val update_query = MongoDBObject("_id" -> conf_id);
+      val update_fields = MongoDBObject(
+        "$set" -> bestValues 
+      );
+      
+      configColl.findAndModify(update_query, update_fields);
+    }
+    configConnection.underlying.close();
+    configColl = null;
   }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,6 +9,8 @@ import org.apache.spark.mllib.recommendation.Rating
 import org.apache.log4j.Logger
 import java.io.File
 import org.apache.spark.mllib.recommendation.ALS
+import com.mongodb.casbah.commons.MongoDBObject
+import com.mongodb.casbah.MongoClient
 
 object Main {
   def main(args: Array[String]) {
@@ -27,67 +29,11 @@ object Main {
       .setAppName("AcmeSupermarketRecommender")
     val sc = new SparkContext(conf)
     
-    // load personal ratings
-
-    val myRatingsRDD = Functions.loadUserRatings(args(0).toInt, sc)
-    
-    // load ratings and products
-
-    val ratings = Functions.loadRatings(sc)
-
-    val products = Functions.loadProducts(sc)
-
-    val numPartitions = 4
-    val training = ratings.filter(x => x._1 <= 3)
-      .values
-      .union(myRatingsRDD)
-      .repartition(numPartitions)
-      .cache()
-    val validation = ratings.filter(x => x._1 > 3 && x._1 <= 4)
-      .values
-      .repartition(numPartitions)
-      .cache()
-    val test = ratings.filter(x => x._1 > 4).values.cache()
-    
-    val numTraining = training.count()
-    val numValidation = validation.count()
-    val numTest = test.count()
-
-    val ranks = List(8, 12)
-    val lambdas = List(1.0, 10.0)
-    val numIters = List(10, 20)
-    var bestModel: Option[MatrixFactorizationModel] = None
-    var bestValidationRmse = Double.MaxValue
-    var bestRank = 0
-    var bestLambda = -1.0
-    var bestNumIter = -1
-    for (rank <- ranks; lambda <- lambdas; numIter <- numIters) {
-      val model = ALS.train(training, rank, numIter, lambda)
-      val validationRmse = Functions.computeRmse(model, validation, numValidation)
-      println("RMSE (validation) = " + validationRmse + " for the model trained with rank = "
-        + rank + ", lambda = " + lambda + ", and numIter = " + numIter + ".")
-      if (validationRmse < bestValidationRmse) {
-        bestModel = Some(model)
-        bestValidationRmse = validationRmse
-        bestRank = rank
-        bestLambda = lambda
-        bestNumIter = numIter
-      }
+    if(args(0).toInt > -1) {
+      makeRecommendation(sc, args(0).toInt);
+    } else {
+      configureParameters(sc, args(0).toInt);
     }
-
-    val testRmse = Functions.computeRmse(bestModel.get, test, numTest)
-
-    println("The best model was trained with rank = " + bestRank + " and lambda = " + bestLambda
-      + ", and numIter = " + bestNumIter + ", and its RMSE on the test set is " + testRmse + ".")
-      
-    val prediction = Functions.predictForUser(bestModel.get, args(0).toInt, sc).collect().sortBy(_.rating).take(10)
-    
-    var index = 1;
-    prediction.foreach { rating =>
-      println("%2d".format(index) + ". " + products.get(rating.product).get)
-      index += 1
-    }
-    // clean up
     sc.stop()
   }
 }
